5558c6d883e284b58ca7153173620309d87116ff
Daniel Pérez García <daniperezg02@gmail.com>
2025-02-17 14:22:25 +0100

    Formulario para Elegir Ruta

--- Web/elegirRuta.php
+++ Web/elegirRuta.php
@@ -4,6 +4,11 @@ require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Elegir Ruta';
 
+$formRuta=new FormularioElegirRuta();
+$htmlForm=$formReg->gestiona();
+
 $contenidoPrincipal = <<<EOS
+    $htmlForm
 EOS;
+
 require __DIR__.'/includes/vistas/plantillas/plantilla.php';
