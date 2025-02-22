58137c62c7a51eaa2775e5e913403310835ee471
Daniel Pérez García <daniperezg02@gmail.com>
2025-02-22 20:20:21 +0100

    Index

--- Web/includes/vistas/comun/cabecera.php
+++ Web/includes/vistas/comun/cabecera.php
@@ -1,15 +1,28 @@
+<?php 
+$pagina = basename($_SERVER['PHP_SELF']);
+$ocultaMenu = ["index.php"]; 
+?>
 <header>
-<div class="logo-container">
-	<img src="<?= RUTA_IMGS; ?>/MetroMadridLogo.png" alt="LogoMetro" id ="LogoMetro">
-	</div>  
+    <?php if (!in_array($pagina, $ocultaMenu)): ?>
 
-    <ul class = "menu">
-        <li><a href="<?= RUTA_APP ?>/index.php">Inicio</a></li>
-        <li><a href="<?= RUTA_APP ?>/lineasMetro.php">Estaciones</a></li>
-        <li><a href="<?= RUTA_APP ?>/acerca.php">Acerca</a></li>
-    </ul>
+    <div class="logo-container">
+    <img src="<?= RUTA_IMGS; ?>/MetroMadridLogo.png" alt="LogoMetro" id ="LogoMetro">
+    </div>  
 
-	<div class="titulo">
-		<h1>Good Stations</h1>
-	</div>
+    <div class="logo-cam">
+    <img src="<?= RUTA_IMGS; ?>/EmblemaCAM.png" alt="LogoCam" id ="LogoCam">
+    </div>
+
+    <nav class="menu">
+        <ul>
+            <li><a href="<?= RUTA_APP ?>/index.php">Inicio</a></li>
+            <li><a href="<?= RUTA_APP ?>/lineasMetro.php">Estaciones</a></li>
+            <li><a href="<?= RUTA_APP ?>/acerca.php">Acerca</a></li>
+        </ul>
+    </nav>
+    <?php endif; ?>
+
+    <div class="titulo">
+        <h1>Good Stations</h1>
+    </div>
 </header>
--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,13 @@ require_once __DIR__.'/includes/config.php';
 $tituloPagina = 'Metro de Madrid';
 
 $contenidoPrincipal = <<<EOS
-    <form action="elegirRuta.php" method="POST">
+    <div class="logo-index">
+    <img src="<?= RUTA_IMGS; ?>/MetroMadridLogo.png" alt="LogoMetro" id ="logoIndex">
+    </div>  
+    <div class="logo-cam-index">
+    <img src="<?= RUTA_IMGS; ?>/EmblemaCAM.png" alt="LogoCam" id ="LogoCamIndex">
+    </div>
+    <form id="botonEntrar" action="elegirRuta.php" method="POST">
         <button type="submit">Elegir Ruta</button>
     </form>
 EOS;
