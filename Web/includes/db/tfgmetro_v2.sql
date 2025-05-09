6ace96fc0b99c9528f93bbcec55bd68fe144443a
Jmari99 <pepedom99@gmail.com>
2025-05-09 23:18:10 +0200

    Add files via upload

---
+++ Web/includes/db/tfgmetro_v2.sql
@@ -0,0 +1,224 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-05-2025 a las 10:28:40
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tfgmetro`
+--
+CREATE DATABASE IF NOT EXISTS `tfgmetro` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tfgmetro`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `conexiones`
+--
+
+DROP TABLE IF EXISTS `conexiones`;
+CREATE TABLE `conexiones` (
+  `id` int(11) NOT NULL,
+  `estacion_origen` int(11) NOT NULL,
+  `estacion_destino` int(11) NOT NULL,
+  `tiempo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estaciones`
+--
+
+DROP TABLE IF EXISTS `estaciones`;
+CREATE TABLE `estaciones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `accesibilidad` tinyint(1) NOT NULL DEFAULT 0,
+  `direccion` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estaciones_lineas`
+--
+
+DROP TABLE IF EXISTS `estaciones_lineas`;
+CREATE TABLE `estaciones_lineas` (
+  `estacion_id` int(11) NOT NULL,
+  `linea_id` int(11) NOT NULL,
+  `orden` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lineas`
+--
+
+DROP TABLE IF EXISTS `lineas`;
+CREATE TABLE `lineas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `siglas` varchar(10) NOT NULL,
+  `color` VARCHAR(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `caracteristicas`
+--
+
+DROP TABLE IF EXISTS `caracteristicas`;
+CREATE TABLE `caracteristicas` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estaciones_caracteristicas`
+--
+
+DROP TABLE IF EXISTS `estaciones_caracteristicas`;
+CREATE TABLE `estaciones_caracteristicas` (
+  `caracteristica_id` int(11) NOT NULL,
+  `estacion_id` int(11) NOT NULL,
+  `valor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transbordos`
+--
+
+DROP TABLE IF EXISTS `transbordos`;
+CREATE TABLE `transbordos` (
+  `id` int(11) NOT NULL,
+  `id_estacion` int(11) NOT NULL,
+  `lineas_trasbordos` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `conexiones`
+--
+ALTER TABLE `conexiones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `estacion_origen_id` (`estacion_origen`),
+  ADD KEY `estacion_destino_id` (`estacion_destino`);
+
+--
+-- Indices de la tabla `estaciones`
+--
+ALTER TABLE `estaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estaciones_lineas`
+--
+ALTER TABLE `estaciones_lineas`
+  ADD PRIMARY KEY (`estacion_id`,`linea_id`),
+  ADD KEY `linea_id` (`linea_id`);
+
+--
+-- Indices de la tabla `lineas`
+--
+ALTER TABLE `lineas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `transbordos`
+--
+ALTER TABLE `transbordos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `estacion_origen_id` (`id_estacion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- Indices de la tabla `caracteristicas``
+--
+ALTER TABLE `caracteristicas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de la tabla `conexiones`
+--
+ALTER TABLE `conexiones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estaciones`
+--
+ALTER TABLE `estaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `lineas`
+--
+ALTER TABLE `lineas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `transbordos`
+--
+ALTER TABLE `transbordos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `caracteristicas`
+--
+ALTER TABLE `caracteristicas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `conexiones`
+--
+ALTER TABLE `conexiones`
+  ADD CONSTRAINT `conexiones_ibfk_1` FOREIGN KEY (`estacion_origen`) REFERENCES `estaciones` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `conexiones_ibfk_2` FOREIGN KEY (`estacion_destino`) REFERENCES `estaciones` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `estaciones_lineas`
+--
+ALTER TABLE `estaciones_lineas`
+  ADD CONSTRAINT `estaciones_lineas_ibfk_1` FOREIGN KEY (`estacion_id`) REFERENCES `estaciones` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `estaciones_lineas_ibfk_2` FOREIGN KEY (`linea_id`) REFERENCES `lineas` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `transbordos`
+--
+ALTER TABLE `transbordos`
+  ADD CONSTRAINT `transbordos_ibfk_1` FOREIGN KEY (`id_estacion`) REFERENCES `estaciones` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
